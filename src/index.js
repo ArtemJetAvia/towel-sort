1e06e18fa2793b180b841c724cef3268ccfba14a
ArtemJetAvia <artemavia87@mail.ru>
2020-10-13 20:03:27 +0300

    task pfmd test passed

--- src/index.js
+++ src/index.js
@@ -3,9 +3,40 @@
 
 module.exports = function towelSort (matrix) {
 
-test
+  let rez=[];
+
+  if (!Array.isArray(matrix)) {
+    return rez;
+  }
+
+  matrix.forEach(function(item, index, array) {
+    if (Array.isArray(item)) {
+      if (index % 2 === 0) {
+      item.forEach(function(item, index, array) {
+        rez.push(item);
+      });
+    } else {
+      item.reverse();
+      item.forEach(function(item, index, array) {
+        rez.push(item);
+      });
+    } 
+    
 
 
 
-  return [];
+  }
+  else {
+    rez.push(item);
+  }
+    
+
+
+});
+
+console.log(matrix);
+console.log(rez);
+  return rez;
+
+
 }
